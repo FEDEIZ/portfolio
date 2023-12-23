fbd16b10def69975b113e2fa543ba2936ebccdcd
Fede <ing.finterlandi@gmail.com>
2023-12-23 18:01:52 -0300

    Contact form with email styles

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,11 +4,12 @@ import SectionHeader from './SectionHeader'
 import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { useSectionInView } from '@/lib/hooks';
+import { sendEmail } from '@/actions/sendEmail';
 
 export default function Contact() {
 
     const { ref } = useSectionInView("Contact");
-    
+
   return (
     <motion.section ref={ref} id='contact' className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'
     initial={{
@@ -31,9 +32,18 @@ export default function Contact() {
             </a>{" "}
             or through this form.
         </p>
-        <form className='mt-10 flex flex-col'>
-            <input placeholder='Your email' className='px-4 h-14 rounded-lg borderBlack' type="email" />
-            <textarea placeholder='Your message...' className='h-52 my-3 rounded-lg borderBlack p-4'/>
+        <form className='mt-10 flex flex-col' 
+        action={async formData => {
+            await sendEmail(formData);
+        }}>
+            <input 
+            name='senderEmail'
+            placeholder='Your email' 
+            className='px-4 h-14 rounded-lg borderBlack' 
+            type="email" 
+            required 
+            maxLength={500}/>
+            <textarea name='message' required maxLength={1000} placeholder='Your message...' className='h-52 my-3 rounded-lg borderBlack p-4'/>
             <button type='submit' className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 
             active:scale-105'>
                 Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1'/>{" "}</button>
