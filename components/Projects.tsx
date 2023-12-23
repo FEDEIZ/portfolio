b29ee0403e1b3a8896da7fc78c5e429223a66d8b
Fede <ing.finterlandi@gmail.com>
2023-12-23 10:15:57 -0300

    skills section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
   
   return (
-    <section id="projects" ref={ref}>
+    <section id="projects" ref={ref} className='scroll-mt-28 mb-28'>
         <SectionHeader>My Projects</SectionHeader>
         <div>
             {
